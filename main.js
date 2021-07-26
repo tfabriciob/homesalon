d7c2aaabee902a6b217a1bd61eb09c88f048cee5
Fabricio Barbosa <fabricio.barbosa@vgxcontactcenter.com.br>
2021-07-26 17:36:34 -0300

    aula 03 concluida

--- main.js
+++ main.js
@@ -65,3 +65,20 @@ const swiper = new Swiper('.swiper-container', {
     mousewheel: true,
     keyboard: true
   });
+
+
+/*ScrollReveal: mostrar elementos quando der scroll na pagina*/
+const scrollReveal = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+    reset: true
+})
+
+scrollReveal.reveal(
+    `#home .image, #home .text,
+    #about .image, #about .text,
+    #services header, #services .card,
+    #testimonials header, #testimonials .testimonials,
+    #contact .text, #contact .links
+    `, { interval: 100 })
