1371439e8410e2650956e7fa1699e5a632559c28
Fabricio Barbosa <fabricio.barbosa@vgxcontactcenter.com.br>
2021-08-02 11:22:25 -0300

    commit final homesalon

--- main.js
+++ main.js
@@ -32,7 +32,6 @@ for (const element of toggle) {
 }
 
 /*quando clicar em algum item do menu, esconder o menu*/
-
 const links = document.querySelectorAll('nav ul li a')
 
 for (const link of links) {
@@ -42,19 +41,18 @@ for (const link of links) {
 }
 
 /*mudar o header da pagina quando der scroll*/
-
 const header = document.querySelector('#header')
 const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function() {
+function changeHeaderWhenScroll() {
     if (window.scrollY >= navHeight) {
-        //scroll é maior que a altura do header
+        // scroll é maior que a altura do header
         header.classList.add('scroll')
     } else {
-        //menor que a altura do header
+        // menor que a altura do header
         header.classList.remove('scroll')
     }
-})
+}
 
 /*Testimonials carousel slider swiper*/
 const swiper = new Swiper('.swiper-container', {
@@ -63,8 +61,14 @@ const swiper = new Swiper('.swiper-container', {
         el: '.swiper-pagination'
     },
     mousewheel: true,
-    keyboard: true
-  });
+    keyboard: true,
+    breakpoints: { //deixar os comentários dois em cada tela quando atingir o tamanho de tablet
+        767: {
+            slidesPerView: 2,
+            setWrapperSize: true
+        }
+    }
+})
 
 
 /*ScrollReveal: mostrar elementos quando der scroll na pagina*/
@@ -79,17 +83,52 @@ scrollReveal.reveal(
     `#home .image, #home .text,
     #about .image, #about .text,
     #services header, #services .card,
-    #testimonials header, #testimonials .testimonials,
+    #testimonials header, #testimonials .testimonials
     #contact .text, #contact .links,
     footer .brand, footer .social
-    `, { interval: 100 })
+    `,
+    { interval: 100 }
+)
 
 /*botão voltar para o topo*/
 const backToTopButton = document.querySelector('.back-to-top')
-window.addEventListener('scroll', function() {
+
+function backToTop() {
     if (window.scrollY >= 560) {
         backToTopButton.classList.add('show')
     } else {
         backToTopButton.classList.remove('show')
     }
+}
+
+/*Menu ativo conforme a sessão visivel na página*/
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+    const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+    for (const section of sections) {
+        const sectionTop = section.offsetTop
+        const sectionHeight = section.offsetHeight
+        const sectionId = section.getAttribute('id')
+
+        const checkpointStart = checkpoint >= sectionTop
+        const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+        if (checkpointStart && checkpointEnd) {
+            document
+                .querySelector('nav ul li a[href*=' + sectionId + ']')
+                .classList.add('active')
+        } else {
+            document
+                .querySelector('nav ul li a[href*=' + sectionId + ']')
+                .classList.remove('active')
+        }
+    }
+}
+
+/* When Scroll */
+window.addEventListener('scroll', function () {
+    changeHeaderWhenScroll()
+    backToTop()
+    activateMenuAtCurrentSection()
 })
